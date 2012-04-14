41addcb6d9a3660281dda8408c0a91b958eb9a08
Alexander Laptev <laptevsacha@mail.ru>
2012-04-14 16:37:47 +0700

    section content type

---
+++ sites/all/themes/drupalsib/template.php
@@ -0,0 +1,14 @@
+<?php
+
+function drupalsib_preprocess_node(&$vars, $hook) {
+  $view_mode = $vars['view_mode'];
+  $node = $vars['node'];
+
+  $preprocess = 'bl_preprocess_node__' . $node->type . '_' . str_replace('-', '_', $view_mode);
+
+  if (function_exists($preprocess)) {
+    $preprocess($vars, $hook);
+  }
+
+  $vars['theme_hook_suggestions'][] = 'node__' . $node->type . '_' . str_replace('-', '_', $view_mode);
+}
