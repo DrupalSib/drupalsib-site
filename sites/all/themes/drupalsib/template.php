99211674fbad9dcd3b464c34cf8cd815275b06cd
Alexander Laptev <laptevsacha@mail.ru>
2012-04-14 16:48:36 +0700

    template.php fixed

--- sites/all/themes/drupalsib/template.php
+++ sites/all/themes/drupalsib/template.php
@@ -4,7 +4,7 @@ function drupalsib_preprocess_node(&$vars, $hook) {
 $view_mode = $vars['view_mode'];
 $node = $vars['node'];
 
-$preprocess = 'bl_preprocess_node__' . $node->type . '_' . str_replace('-', '_', $view_mode);
+$preprocess = 'drupalsib_preprocess_node__' . $node->type . '_' . str_replace('-', '_', $view_mode);
 if (function_exists($preprocess)) {
 $preprocess($vars, $hook);
 }
