206768e9f028ebf6c8b1329fce5d39c7be999b95
Manyakhin Roman <@MyAcerPC.(none)>
2012-04-15 10:38:44 +0700

    Userbar

--- sites/all/modules/custom/drupalsib_site/plugins/content_types/logobar/drupalsib_site_logobar.tpl.php
+++ sites/all/modules/custom/drupalsib_site/plugins/content_types/logobar/drupalsib_site_logobar.tpl.php
@@ -2,11 +2,13 @@
   <div class="ds_header_top">
     <div class="ds_header_logo">
       <a href="/">
-        <img src="/<?php print DRUPALSIB_SITE_THEME_PATH; ?>/images/logo.png" alt="logo"/>
+        <?php 
+          print $site_logo; 
+        ?>
       </a>
-      <span>
-        <?php print $site_slogan; ?>
-      </span>
+      <?php 
+        print $site_slogan; 
+      ?>
     </div>
   </div>
 </div>
